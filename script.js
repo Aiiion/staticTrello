29ed80cc80ace1a35eb50f206c32da7c414c3c76
Alex <alex.soderberg97@gmail.com>
2021-03-09 15:48:20 +0100

    now all cards have dialog and tabs

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ $( function() {
   } );
 
 $( function(){
-    $('div#dialog1').dialog({autoOpen: false })
-    $('#btn1').click(function(){$('div#dialog1').dialog('open');})
+    $('.dialog').dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 500
+      },
+      hide: {
+        effect: "fold",
+        duration: 500
+      } 
+    });
+    $('.btn').on("click", function(){
+      let id = $(this).attr('data-id');
+      $(id).dialog('open');})
 });
+
+$( function() {
+  $( ".dialog" ).tabs();
+  
+} );
+
+
+$( function() {
+  $( ".datepicker" ).datepicker();
+} );
