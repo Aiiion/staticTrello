4ce5fe528f1c852b94501b7c2f8947afa1204b6b
Alex <alex.soderberg97@gmail.com>
2021-03-10 23:22:35 +0100

    minor fixes, still needs widget

--- script.js
+++ script.js
@@ -33,18 +33,27 @@ $(function(){
     $("#audio")[0].play();
   })
 } )
-$(function(){
+$(function(){ //Updates progress bar based on how many cards is sorted as done
   $(".ui-sortable-handle").hover(function(){
     let progress = 0;
   $("#done").find(".ui-sortable-handle").each(function(){
     progress += 25;
     console.log($(".ui-sortable-handle").attr("id"));
-    console.log(progress);
-    
+    console.log(progress);    
   })
   $(".progress-bar").attr('aria-valuenow', progress);
     $(".progress-bar").css('width', progress + "%");
-  })
-  
-  
+    if ($(".progress-bar").attr('aria-valuenow') == 100) {
+      $("#dunder").css("display", "block");
+    } else {
+      $("#dunder").css("display", "none");
+    }
+  }) 
+})
+$(function(){
+  if ($(".progress-bar").attr('aria-valuenow') == 100) {
+    $("#dunder").css("display", "block");
+  } else {
+    $("#dunder").css("display", "none");
+  }
 })
