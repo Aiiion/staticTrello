16aefd333930ea7538fe552da96d1f833f68cea1
Alex <alex.soderberg97@gmail.com>
2021-03-11 18:04:43 +0100

    added widget, minor fixes

--- script.js
+++ script.js
@@ -27,9 +27,40 @@ $( function() {
 $( function() {
   $( ".datepicker" ).datepicker();
 } );
-
 $(function(){
-  $("#dunder").on("click", function(){
+  $.widget("change.colors", {
+    options: {
+      red:5,
+      green:5,
+      blue:150,
+      opacity: .5
+    },
+    _create: function(){
+      $(".container").addClass("colorChanger");
+      this.changer = $("<button>", {
+        text: "Engadge exitement",
+        "class": "changeBodyColor btn-lg btn-danger col-xl-12"
+      })
+      .appendTo(this.element).button();
+      this._on(this.changer, {
+        click: "emulateWormhole"
+      });
+    },
+    emulateWormhole: function(event){
+      this.element.css( "background-color", "rgba(" +
+          this.options.red +"," +
+          this.options.green + "," +
+          this.options.blue + "," +
+          this.options.opacity + ")"
+        );
+    }
+    
+
+  });
+  $("#body").colors();
+})
+$(function(){
+  $(".changeBodyColor").on("click", function(){
     $("#audio")[0].play();
   })
 } )
@@ -44,16 +75,19 @@ $(function(){ //Updates progress bar based on how many cards is sorted as done
   $(".progress-bar").attr('aria-valuenow', progress);
     $(".progress-bar").css('width', progress + "%");
     if ($(".progress-bar").attr('aria-valuenow') == 100) {
-      $("#dunder").css("display", "block");
+      $(".changeBodyColor").css("display", "block");
     } else {
-      $("#dunder").css("display", "none");
+      $(".changeBodyColor").css("display", "none");
     }
   }) 
 })
+
+
+
 $(function(){
   if ($(".progress-bar").attr('aria-valuenow') == 100) {
-    $("#dunder").css("display", "block");
+    $(".changeBodyColor").css("display", "block");
   } else {
-    $("#dunder").css("display", "none");
+    $(".changeBodyColor").css("display", "none");
   }
 })
